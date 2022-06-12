d1e0808df755e0e1a4784073cd669738ad9bac88
Aljamil1113 <aarazasaljamil@gmail.com>
2022-06-13 01:12:38 +0800

    Client - Filter products by brand and type

--- skinet/client/src/app/shop/shop.component.ts
+++ skinet/client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../models/brand';
 import { IProduct } from '../models/product';
+import { IType } from '../models/type';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,11 +11,21 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products!: IProduct[];
+  brands!: IBrand[];
+  types!: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe({
       next: (response => {
          this.products = response.data
       }),
@@ -21,6 +33,38 @@ export class ShopComponent implements OnInit {
         console.log(err);
       }
     })
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: (response => {
+         this.brands = [{id: 0, name: 'All'}, ...response];
+      }),
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: (response => {
+         this.types = [{id: 0, name: 'All'}, ...response];
+      }),
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 
 }
--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { IBrand } from '../models/brand';
 import { IPagination } from '../models/pagination';
+import { IType } from '../models/type';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,28 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if(brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId) {
+      params = params.append('typeid', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(this.baseUrl + 'products', {params: params, observe: "body"});
+    // .pipe(delay(1000), map(response => {
+    //   return response.body
+    // }));
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
