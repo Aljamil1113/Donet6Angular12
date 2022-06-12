8358d4a5b32cd3c56ee9ce06651ee23a7d70ac14
Aljamil1113 <aarazasaljamil@gmail.com>
2022-06-12 12:03:39 +0800

    Add Interface model in angular

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,30 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Skinet';
-  products!: any[];
-  
 
-  constructor(private http: HttpClient) {}
-  
+export class AppComponent implements OnInit {
+  title = 'SkiNet';
+  products!: IProduct[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7023/api/products').subscribe({
+    this.http.get('https://localhost:7023/api/products?pageSize=50').subscribe({
       next: (response: any) => {
-       this.products = response.data;
+        this.products = response.data;
       },
       error: (err) => {
         console.log(err);
       }
-    }); 
-
-    
+    })
   }
-  
-}
+} 
